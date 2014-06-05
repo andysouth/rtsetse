9d2b16b3b1161fe4bd0d1a758dcd703ac7a1094e
Andy South <southandy@gmail.com>
2014-06-05 18:02:07 +0100

    simple ageing & movement working

--- R/rtPhase2Test.r
+++ R/rtPhase2Test.r
@@ -6,8 +6,8 @@
 
 #! check if it works when nRow & nCol == 1 (the aspatial model)
 
-nRow <- 1 #0
-nCol <- 1 #0
+nRow <- 10
+nCol <- 10
 nAge <- 7
 iDays <- 4
 
@@ -16,24 +16,35 @@ dimnames <- list(NULL,NULL,NULL)
 names(dimnames) <- c("x","y","age")
 aF <- array(0, dim=c(nCol,nRow,nAge), dimnames=dimnames)
 #adding 100 females of age 3 at 5,5
-#aF[5,5,3] <- 100
+aF[5,5,3] <- 100
 #also I can easily fill a constant age structure
 #aF[5,5,] <- 100
 #to test the aspatial model
-aF[1,1,3] <- 100
+#aF[1,1,3] <- 100
 
 #to get a matrix for one age class
 #aF[,,3]
 
 for( day in 1:iDays ) {
 
+  ##################
   ## adult ageing ##
   #vPopF <- rtAgeing(vPopF, label="adult F")
   #I now want to apply for all matrix cells
   #aOut <- aaply(aF, .margins=3, rtMove1 )
-  aF <- aaply(aF, .margins=3, rtAgeing, label="adult F")
-  #putting array compnents back in correct order
+  
+  #.drop=FALSE makes it work for 1x1 grid
+  aF <- aaply(aF, .margins=c(1,2), .drop=FALSE, rtAgeing )
+  
+  ##############
+  ## movement ##
+  aF <- aaply(aF, .margins=3, rtMove1 )
+  #putting array components back in correct order
   aF <- aperm(aF, c(2, 3, 1))
+  
+  #cat("day",day,"\n")
+  
+  aF
   
 } #end of iDays loop
 
