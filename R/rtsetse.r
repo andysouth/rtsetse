c985d7ce5ec426eff7644066de06042809b48598
Andy South <southandy@gmail.com>
2014-06-11 13:42:41 +0100

    adding package documentation file

---
+++ R/rtsetse.r
@@ -0,0 +1,9 @@
+#' simulating tsetse fly populations
+#'
+#' rtsetse provides functions to create tsetse fly population simulations
+#'
+
+#' @import sp raster plyr
+#' @docType package
+#' @name rtsetse
+NULL
