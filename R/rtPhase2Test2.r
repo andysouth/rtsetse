071cd262723916198449ee06d6aead7f182b89c3
Andy South <southandy@gmail.com>
2014-07-04 16:56:11 +0100

    Put pupae into rtPhase2Test2

--- R/rtPhase2Test2.r
+++ R/rtPhase2Test2.r
@@ -15,10 +15,12 @@
 #' @param iCarryCap carrying capacity of adults 
 #' @param iStartAdults number of adults to start simulation with
 #' @param iStartAges spread start adults across the first n ages classes
+#' @param iStartPupae number of pupae to start simulation with (they get spread across sex&age)
 #' @param pMortF adult female mortality per day 
 #' @param pMortM adult male mortality per day 
 #' @param loop TEMPORARY test of loop versus apply approach, only TRUE works 
-
+#' @param iPupDurF days it takes pupa(F) to develop
+#' @param iPupDurM days it takes pupa(M) to develop
 
 #' @return a multi-dimensional array [day,x,y,sex,ages]
 #' @examples
@@ -34,9 +36,12 @@ rtPhase2Test2 <- function(
                           iCarryCap = 200,
                           iStartAdults = 200,
                           iStartAges = 1,
+                          iStartPupae = 200,
                           pMortF = 0.05,
                           pMortM = 0.05,
-                          loop = TRUE)
+                          loop = TRUE,
+                          iPupDurF = 26,
+                          iPupDurM = 28 )
 {
   
   ##some argument checking
@@ -48,21 +53,43 @@ rtPhase2Test2 <- function(
   vpMortF <- rep(pMortF,iMaxAge) 
   vpMortM <- rep(pMortM,iMaxAge) 
   
-  #also I can easily fill a constant age structure
-  #aF[5,5,] <- 100
-  
-  #starting to exlplore whether I can have an array with M&F
-  #to pass to mortality function
+    
+  #ADULTS
   # 'x','y','sex','age'
-  #dimnames <- list(NULL,NULL,NULL,NULL)
   #dimnames <- list(1:nCol,1:nRow,c("F","M"),1:iMaxAge)
   dimnames1 <- list( paste0('x',1:nCol), paste0('y',1:nRow), c("F","M"), paste0('age',1:iMaxAge))
   names(dimnames1) <- c("x","y","sex","age")
   aGrid <- array(0, dim=c(nCol,nRow,2,iMaxAge), dimnames=dimnames1)  
-  #this might fill in both M&F
   #adding half of starting adults as each gender to starting cell
   #2 params allow number and distribution of flies across age classes to be set
   aGrid[(nCol+1)/2, (nRow+1)/2, , 1:iStartAges] <- iStartAdults/(2*iStartAges)
+  
+  #aF[5,5,] <- 100 #could be used easily to fill a constant age structure
+  
+  #PUPAE 
+  #similar array to adults except numAges is different
+  #and can potentially be different betwenn M&F
+  iMaxPupAge <- max(iPupDurM, iPupDurF)
+  dimnamesPup <- list( paste0('x',1:nCol), paste0('y',1:nRow), c("F","M"), paste0('age',1:iMaxPupAge))
+  names(dimnamesPup) <- c("x","y","sex","age")
+  aGridPup <- array(0, dim=c(nCol,nRow,2,iMaxPupAge), dimnames=dimnamesPup)  
+  #pupae are done slightly differently from adults
+  #it puts indivs into all age classes
+  #because males stay in the ground longer this means there will be more males
+  #next 3 lines same code as from rtPhase1Test2.r
+  fPupaPerSexAge <- iStartPupae/(iPupDurF+iPupDurM)
+  vPupaM <- rep(fPupaPerSexAge, iPupDurM)
+  #make the F vector up to the same length as the M with extra 0's
+  #vPupaF <- rep(fPupaPerSexAge, iPupDurF)
+  vPupaF <- c(rep(fPupaPerSexAge, iPupDurF),rep(0,iPupDurM-iPupDurF))
+  #then put each pupal vector into the array
+  aGridPup[(nCol+1)/2, (nRow+1)/2,'F', ] <- vPupaF
+  aGridPup[(nCol+1)/2, (nRow+1)/2,'M', ] <- vPupaM
+  
+  
+  
+
+  
   
   #create a matrix for carrying capacity on the grid
   #first test make it constant
@@ -98,15 +125,9 @@ rtPhase2Test2 <- function(
     
     ###############
     ## mortality ##
-    #this is how it's done in rtPhase1Test
-    #!beware of potential bug of passing vectors in wrong order 
-    #(remember ladies first) or do it a different way
-    #lPop <- rtMortality( vPopF, vPopM, vpMortF, vpMortM, 
-    #                     propDD = propMortAdultDD,
-    #                     iCarryCap = iCarryCap) 
-    #vPopF <- lPop$vFem
-    #vPopM <- lPop$vMal  
     
+    #! probably want to remove the carryCap bit of this
+    #! if density dependence is going to be implemented purely through pupal mort
     aGrid <- rtMortalityGrid( aGrid, vpMortF, vpMortM, mCarryCap=mCarryCap, loop=loop )
     
     
