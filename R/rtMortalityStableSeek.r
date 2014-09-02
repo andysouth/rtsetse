3863aaebe48c7aca6e502f397b78f138f64015dc
Andy South <southandy@gmail.com>
2014-09-02 15:09:27 +0100

    refactoring stability seek

--- R/rtMortalityStableSeek.r
+++ R/rtMortalityStableSeek.r
@@ -4,29 +4,60 @@
 #' early development
 
 #' @param iMaxAge max age of fly allowed in model
-#' @param fPopAge1 starting popn at age1
+#' @param iTargetPopAge0 target pop of both sexes at age 0
 #' @param fInterval the interval between mortalities used to test
 #' @param fMperF desired numbers of males per female, default=0.5
 #' @param pMortLarva larval mortality per period
+#' 
+#' @param iMortMinAgeStart  Age at which min death rates start. 
+#' @param iMortMinAgeStop   Age at which min death rates stop.
+#' @param fMortMinProp  What proportion of the maximum death rate on day 0 is the minimum death rate.
+#' @param fMortOldProp  What proportion of the maximum death rate on day 0 is the death rate after iDeathMinAgeStop.
+#' @param propMortAdultDD proportion of adult mortality that is density dependent
 #' @param pMortPupa pupal mortality per period
+#' @param iPupDurF days it takes pupa(F) to develop
+#' @param iPupDurM days it takes pupa(M) to develop
+#' @param iFirstLarva Age that female produces first larva
+#' @param iInterLarva Inter-larval period 
+#' 
+#' @param plot whether to output plots
+#' @param verbose whether to output progress text
 #' 
 #' #' @return float mortality probability
 #' @export
 
 
 rtMortalityStableSeek <- function( iMaxAge = 100,
-                                  fPopAge1 = 100,
+                                  iTargetPopAge0 = 100,
                                   fInterval = 0.001,
                                   fMperF = 0.5,
                                   pMortLarva = 0.05,
-                                  pMortPupa = 0.25,
-                                  plot =  TRUE )
+                                  
+                                 iMortMinAgeStart = 10,
+                                 iMortMinAgeStop = 50,
+                                 fMortMinProp = 0.2,
+                                 fMortOldProp = 0.3,
+                                 propMortAdultDD = 0.25,
+                                 
+                                 pMortPupa = 0.25,
+                                 iPupDurF = 26,
+                                 iPupDurM = 28,
+                                 iFirstLarva = 16,
+                                 iInterLarva = 10, 
+    
+                                  plot = TRUE,
+                                  verbose = FALSE )
 {
   
   #create a vector of mortalities to test both M&F
   #starts low & increases
   #unlikely to ever go above mort 0.4 (or even get close to)
   vMorts <- seq(fInterval, 0.4, fInterval) 
+  
+  #impose pupal mort to get adults of age1 (division by 2 for both sexes)
+  popAge1MorF <- iTargetPopAge0/2 * (1-pMortPupa) 
+  #test with no pupal mort actually gave same results
+  #popAge1MorF <- iTargetPopAge0/2  
   
   ########
   #Females
@@ -35,10 +66,10 @@ rtMortalityStableSeek <- function( iMaxAge = 100,
   vLarvae <- rep(NA,length(vMorts))
   
   #set trialLarvae high to start the while loop
-  numTrialLarvae <- fPopAge1+1
+  numTrialLarvae <- popAge1MorF+1
   trial <- 1
   #try increasing mortalities while trial is above threshold
-  while( numTrialLarvae > fPopAge1 )
+  while( numTrialLarvae > popAge1MorF )
   {
     #set age1 mort for this trial
     fMort <- vMorts[trial]
@@ -49,15 +80,17 @@ rtMortalityStableSeek <- function( iMaxAge = 100,
     
     #fill an age structure
     #todo: decide whether to add pupal mort before or after here
-    vPopF <- rtSetAgeStructure( vpMort=vpMort, fPopAge1=fPopAge1 )
+    vPopF <- rtSetAgeStructure( vpMort=vpMort, fPopAge1=popAge1MorF )
     
     #setting age dependent mortality rates
-    vpDeposit <- rtSetDepositionRatesByAgeDI( iMaxAge=iMaxAge, pMortLarva=pMortLarva )
+    vpDeposit <- rtSetDepositionRatesByAgeDI( iMaxAge=iMaxAge, iFirstLarva=iFirstLarva, iInterLarva=iInterLarva, pMortLarva=pMortLarva )
     #calc M&F larvae using the deposition rates set above
     lLarvae <- rtLarvalDeposition( vPopF, vpDeposit )    
     #summing M&F
     numTrialLarvae  <- lLarvae$iLarvaeF + lLarvae$iLarvaeM  
-        
+    
+    if (verbose) cat("trial MortF:",fMort,"totF:",sum(vPopF),"larvae:",numTrialLarvae,"\n")
+    
     vLarvae[trial] <- numTrialLarvae
     trial <- trial+1
   }
@@ -71,7 +104,7 @@ rtMortalityStableSeek <- function( iMaxAge = 100,
   {
     plot( vMorts, vLarvae, type='l', ylab='larvae' )
     #add starting pop age1
-    abline(h=fPopAge1, col='red')    
+    abline(h=popAge1MorF, col='red')    
   }
   
   
@@ -99,10 +132,12 @@ rtMortalityStableSeek <- function( iMaxAge = 100,
     vpMort <- rep(fMort, iMaxAge)
     #fill an age structure
     #todo: decide whether to add pupal mort before or after here
-    vPopM <- rtSetAgeStructure( vpMort=vpMort, fPopAge1=fPopAge1 )
+    vPopM <- rtSetAgeStructure( vpMort=vpMort, fPopAge1=popAge1MorF )
     
     #sum num males in all age classes
     numTrialMales <- sum(vPopM)
+    
+    if (verbose) cat("trial MortM:",fMort,"totM:",numTrialMales,"\n")
     
     vMales[trial] <- numTrialMales
     trial <- trial+1
