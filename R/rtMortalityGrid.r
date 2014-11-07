00eb53cbcfd7c19c93bfb16276aca74e1d03cf21
Andy South <southandy@gmail.com>
2014-11-07 15:08:31 +0000

    add mortality multiplier grid

--- R/rtMortalityGrid.r
+++ R/rtMortalityGrid.r
@@ -10,6 +10,7 @@
 #' @param vpMortF a vector of age-specific mortality probabilities of Females 
 #' @param vpMortM a vector of age-specific mortality probabilities of Males 
 #' @param propDD proportion of mortality that is density dependent 
+#' @param mMortMultGrid a grid of mortality multipliers (derived from vegetation)
 #' @param mCarryCap a matrix of Carrying Capacities for each cell as an integer (an alternative to iCarryCap)
 #' @param iCarryCap a single integer Carrying Capacities for all cells as an integer (an alternative to mCarryCap)
 #' 
@@ -20,13 +21,17 @@ rtMortalityGrid <- function( aGrid,
                          vpMortF,
                          vpMortM,
                          propDD = 0.25,
+                         mMortMultGrid = NULL,
                          mCarryCap = NULL,
                          iCarryCap = NULL ) 
 {
  
   #aGrid[x,y,sex,age]
 
+  #checks
   if (is.null(iCarryCap) & is.null(mCarryCap)) stop("you must specify one of either mCarryCap or iCarryCap")
+  
+  #TODO check that the mortMult grid matches dimensions of the array
   
     
   for(x in seq_along(dimnames(aGrid)$x)){
@@ -43,8 +48,21 @@ rtMortalityGrid <- function( aGrid,
       if ( is.null(iCarryCap) ) iCarryCap <- mCarryCap[y,x]  
       
       
-      #only apply mortality if there are flies in the cell
-      #the condition is not essential but should save time
+      #apply mortality multiplier for this cell
+      if ( !is.null(mMortMultGrid) )
+      {
+        iMortMult <- mMortMultGrid[y,x]
+        if (iMortMult != 100)
+        {
+          #first convert percent to proportion
+          iMortMult <- iMortMult/100
+          vpMortF <- vpMortF*iMortMult
+          vpMortM <- vpMortM*iMortMult        
+        }
+      }
+      
+      
+      #only apply mortality if flies in cell (to save time)
       if ( sum(aGrid[x,y,,]) > 0 )
       {
         aGrid[x,y,,] <- rtMortality(vFem = aGrid[x,y,'F',],
