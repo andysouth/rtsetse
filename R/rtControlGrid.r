2ca0c85069b2f5fa971c88e1d9e5575c64eba623
Andy South <southandy@gmail.com>
2015-01-21 18:13:16 +0000

    adding age & sex dependent control

--- R/rtControlGrid.r
+++ R/rtControlGrid.r
@@ -4,7 +4,12 @@
 #' In a central control area.  
 
 #' @param aGrid array of y,x,sex,age
-#' @param pControl proportion of adults to be killed by control (daily)
+#' @param vpControlF proportion by age of F to be killed by control (daily)
+#' age & sex dependent alternative to pControl
+#' @param vpControlM proportion by age of M to be killed by control (daily)
+#' age & sex dependent alternative to pControl
+#' @param pControl control independent of age & sex, proportion of adults to be killed by control (daily)
+#' age & sex INdependent alternative to vpControlF & vpControlM
 #' @param mGridControlModifier an optional grid to modify control. 
 #' The control probability is multiplied by this. SO can use 0,1 grid for control/no control option.
 #' @param iControlBorder the number of cells around the edge where no control applied
@@ -15,17 +20,27 @@
 #' @return aGrid updated array of y,x,sex,age
 #' @examples
 #' aGrid <- rtCreateGrid(nY=3,nX=4,nAge=2,fill=1)
-#' aGridControlled <- rtControlGrid(aGrid) 
-#' aGridControlled <- rtControlGrid(aGrid, iControlBorder=1) 
+#' aGridControlled <- rtControlGrid(aGrid, pControl=0.1) 
+#' aGridControlled <- rtControlGrid(aGrid, pControl=0.1, iControlBorder=1) 
+#' #testing age & sex dependent control
+#' aGridControlled <- rtControlGrid(aGrid, vpControlF=c(0.1,0.2), vpControlM=c(0.3,0.4))
 #' @export
 
 rtControlGrid <- function(aGrid, 
-                               pControl=0.1, 
-                               mGridControlModifier=NULL,
-                               iControlBorder=0, 
-                               verbose = TRUE)
+                          vpControlF = NULL,
+                          vpControlM = NULL,
+                          pControl = NULL,
+                          mGridControlModifier=NULL,
+                          iControlBorder=0, 
+                          verbose = TRUE)
 {
 
+  #TODO check that the dimensions of vpControlF & M match aGrid
+  
+  #another check
+  if ( !is.null(vpControlF) & !is.null(vpControlM) & !is.null(pControl) )
+    warning("You have specified both age dependent (vpControlF & M) and independent (pControl) control, the former will be used\n")
+  
   
   nY <- dim(aGrid)[1]
   nX <- dim(aGrid)[2]
@@ -67,11 +82,50 @@ rtControlGrid <- function(aGrid,
   
   #TODO I probably want to put some of the below into a function on it's own
   
-  #for each cell pop = pop - (pop*controlMort*controlModifier) 
-  #but this actually passes the control modifier as a grid (mGridControl)
-  aGrid <- plyr::aaply(aGrid,.margins=c(3,4), .drop=FALSE,function(m) (m-(m*pControl*mGridControl)))
-  #put array dimensions back in correct order
-  aGrid <- aperm(aGrid, c(3,4,1,2))
+  if ( !is.null(vpControlF) & !is.null(vpControlM))
+  {    
+    #age & sex dependent control
+    
+    #testing const mort just on F : seems to work
+  #   aGridF <- aGrid[,,"F",]
+  #   aGridF2 <- plyr::aaply(aGridF,.margins=c(3), .drop=FALSE,function(m) (m-(m*pControl*mGridControl)))
+  #   aGridF2 <- aperm(aGridF2, c(2,3,1))
+    
+    #BUT another way of doing could be to call rtMortality()
+    for(x in seq_along(dimnames(aGrid)$x)){
+      for(y in seq_along(dimnames(aGrid)$y)){
+        #only apply mortality if flies in cell (to save time)
+        if ( sum(aGrid[y,x,,]) > 0 )
+        { 
+          #modify the age & sex mortality vectors by the grid
+          vpMortF <- vpControlF * mGridControl[y,x]
+          vpMortM <- vpControlM * mGridControl[y,x]
+          
+          #apply mortality for this cell
+          aGrid[y,x,,] <- rtMortality(vFem = aGrid[y,x,'F',],
+                                      vMal = aGrid[y,x,'M',],
+                                      vpMortF = vpMortF,
+                                      vpMortM = vpMortM,
+                                      returnArray = TRUE,
+                                      propDD = 0 )  
+        }#end flies in cell
+      }#end y
+    }#end x
+  #end age & sex dependent control  
+  } else if (! is.null(pControl))
+  {  
+    #if control is specified as a single age/sex independent value
+  
+    #for each cell pop = pop - (pop*controlMort*controlModifier) 
+    #control modifier passed as a grid (mGridControl)
+    aGrid <- plyr::aaply(aGrid,.margins=c(3,4), .drop=FALSE,function(m) (m-(m*pControl*mGridControl)))
+    #put array dimensions back in correct order
+    aGrid <- aperm(aGrid, c(3,4,1,2))
+    
+  } else 
+  {
+    stop("you need to specify either a single control mortality or age & sex dependent vectors")
+  }
   
   
   fFliesAfterControl <- sum(aGrid)
--- tests/testthat/testControl.r
+++ tests/testthat/testControl.r
@@ -52,3 +52,34 @@ test_that("control works when age-independent & varies across a  passed grid", {
                 aGrid[2,3,'F',1] - ( aGrid[2,3,'F',1] * mGridControlModifier[2,3] * pControl )  )   
   
 })
+
+
+test_that("age & sex dependent control works", {
+  
+  #create a test grid array, y,x,sex,age
+  nX <- 4
+  nY <- 3
+  iMaxAge <- 2
+  #create a grid filled with 1s
+  aGrid <- rtCreateGrid(nY=nY, nX=nX, nAge=iMaxAge, fill=1)  
+  #create a control modifying matrix
+  #start with all 1s
+  #mGridControlModifier <- matrix(1,nrow=nY,ncol=nX)
+  #mGridControlModifier[2,2] <- 0.5
+  #mGridControlModifier[2,3] <- 2
+  
+  #apply control modified by age & sex
+  #F 1,2 = 0.1,0.2 M 1,2 = 0.3, 0.4
+  aGrid2 <- rtControlGrid(aGrid, vpControlF=c(0.1,0.2), vpControlM=c(0.3,0.4))
+  
+  #controlled should be  
+  #F age1
+  expect_equal( unique(as.vector(aGrid2[,,'F',1])), 0.9 )  
+  #F age2
+  expect_equal( unique(as.vector(aGrid2[,,'F',2])), 0.8 )  
+  #M age1
+  expect_equal( unique(as.vector(aGrid2[,,'M',1])), 0.7 )  
+  #M age2
+  expect_equal( unique(as.vector(aGrid2[,,'M',2])), 0.6 )                         
+  
+})
