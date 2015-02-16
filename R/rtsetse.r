53b5f9be24e41aa4c028535893fbada9a61c2bef
AndySouth <southandy@gmail.com>
2015-02-16 16:55:29 +0000

    improved in-line package documentation

--- R/rtsetse.r
+++ R/rtsetse.r
@@ -1,11 +1,23 @@
-#' simulating tsetse fly populations
+#' simulating tsetse fly populations to investigate control options.
 #'
-#' rtsetse provides functions to create tsetse fly population simulations  
+#' rtsetse is being developed to provide functions to create tsetse fly population simulations with age, sex and spatial structure.  
+#'
+#' The main functions to run simulations are :
+#'  \describe{
+#'   \item{rt_runAspatial()}{age-structured a-spatial population simulation}
+#'   \item{rt_runGridTestSpread()}{grid simulation, homogenous landscape, spread fro central populated cell}
+#'   \item{rt_runGrid()}{grid simulation, grid simulation, landscape set from vegetation map, population starts at carrying capacity}
+#' }
 #' 
-#' Note that package functions refernced using :: don't need to go here.
-#' Just need to go in DESRIPTION. This determines what imports go in NAMESPACE.
+#' @author
+#' Andy South \email{southandy@@gmail.com} 
+
 #' @import raster sp ggplot2
 #' @importFrom dplyr group_by summarise %>%
 #' @docType package
 #' @name rtsetse
+#' @seealso 
+#'  \url{http://andysouth.shinyapps.io/shinytse7/} demonstration user interface   
+#'  \url{http://andysouth.github.io/rtsetse-intro/} blog post outlining the simulation   
+#'  \url{http://andysouth.github.io/grid-movement/} blog post decsribing how movement is simulated   
 NULL
