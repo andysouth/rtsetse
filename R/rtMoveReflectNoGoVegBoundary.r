27e3a4bfecbfd3415a2a82207c3c4f6fc931a7eb
AndySouth <southandy@gmail.com>
2015-03-06 11:15:12 +0000

    add coping with nogo areas plus comments

--- R/rtMoveReflectNoGoVegBoundary.r
+++ R/rtMoveReflectNoGoVegBoundary.r
@@ -140,12 +140,21 @@ rtMoveReflectNoGoVegBoundary <- function(m = array(c(0,0,0,0,1,0,0,0,0,0,0,0),di
   #default decrease in movement from Hat-trick is
   #1, 0.3, 0.1, 0.03, 0.01, 0.001 
   
+  ################################################################
+  # to next line of # can be put outside the function
+  # then perhaps I pass the following array 
+  #aVegDifMult[y,x,(N,E,S,W,SN,WE,NS,EW)]
   
-  #convert veg characters to numeric
-  mVegNum <- rtSetGridFromVeg( mVegCats, dfLookup=data.frame(from=c("D","T","O","S","B","G","N"),to=c(1,2,3,4,5,6,999),stringsAsFactors = FALSE ))
+  #convert veg characters to numeric, note nogo "N" to NA
+  mVegNum <- rtSetGridFromVeg( mVegCats, dfLookup=data.frame(from=c("D","T","O","S","B","G","N"),to=c(1,2,3,4,5,6,NA),stringsAsFactors = FALSE ))
 
   #matrix of difference of veg in cell from best
   mVegDifPref <- abs(iBestVeg-mVegNum)
+  
+  #BEWARE what to do with nogo areas
+  #try to keep it simple, it shouldn't matter because no flies in them.
+  #can I just convert them to NA ?
+  #NO this wouldn't work ... convert any difference >5 (caused by nogo areas of 99)to 6
   
   #remember
   #in the code below matrices with NESW on end are source cells
@@ -166,24 +175,36 @@ rtMoveReflectNoGoVegBoundary <- function(m = array(c(0,0,0,0,1,0,0,0,0,0,0,0),di
   #unique(as.vector(mVegDifPrefN))
   #now convert the change associated with the move to a modifier of the movement rate
   #convert 0:5 to   1, 0.3, 0.1, 0.03, 0.01, 0.001 
+  #dfLookup <- data.frame(from=c(-5:5),to=c(0.001, 0.01, 0.03, 0.1, 0.3, 1, 1, 1, 1, 1, 1))
+  #include converting any NAs to 0 so no flies move to or from
+  dfLookup <- data.frame(from=c(NA,-5:5),to=c(0, 0.001, 0.01, 0.03, 0.1, 0.3, 1, 1, 1, 1, 1, 1))
   #mVegbmult mVeg boundary multiplier
-  #todo write dryer version of this & previous similar actions
-  mVegbmultN <- rtSetGridFromVeg( mVegDifPrefN, dfLookup=data.frame(from=c(-5:5),to=c(0.001, 0.01, 0.03, 0.1, 0.3, 1, 1, 1, 1, 1, 1)))
-  mVegbmultE <- rtSetGridFromVeg( mVegDifPrefE, dfLookup=data.frame(from=c(-5:5),to=c(0.001, 0.01, 0.03, 0.1, 0.3, 1, 1, 1, 1, 1, 1)))
-  mVegbmultS <- rtSetGridFromVeg( mVegDifPrefS, dfLookup=data.frame(from=c(-5:5),to=c(0.001, 0.01, 0.03, 0.1, 0.3, 1, 1, 1, 1, 1, 1)))
-  mVegbmultW <- rtSetGridFromVeg( mVegDifPrefW, dfLookup=data.frame(from=c(-5:5),to=c(0.001, 0.01, 0.03, 0.1, 0.3, 1, 1, 1, 1, 1, 1)))
+  #these are used for calculating arrivers
+  #for each cell they are the difference in preference with 4 neighbours that act as sources
+  mVegbmultN <- rtSetGridFromVeg( mVegDifPrefN, dfLookup=dfLookup )
+  mVegbmultE <- rtSetGridFromVeg( mVegDifPrefE, dfLookup=dfLookup )
+  mVegbmultS <- rtSetGridFromVeg( mVegDifPrefS, dfLookup=dfLookup )
+  mVegbmultW <- rtSetGridFromVeg( mVegDifPrefW, dfLookup=dfLookup )
   
-  #these go the opposite way
-  #and all boundary values are replaced with 1
-  #because for reflecting boundaries there will be no change in vegetation 
-  #associated with movements in and out of the grid    
-
+  #BEWARE 5/3/15 THIS IS THE TRICKIEST BIT IN THE WHOLE OF RTSETSE
+  #I've tested that it does do what is expected, e.g. see movement vignette
+  #but how the mechanism is coded to be time efficient is very tricky
+  
+  #the below are needed for calculating stayers
+  #for each cell they are the difference in preference with 4 neighbours that act as sinks
+  #they are calculated by going back the other way from the previous calculation
+  #all boundary values are replaced with 1 because for reflecting boundaries there will be no change 
+  #in vegetation associated with movements in and out of the grid    
   mVegbmultSN <- shiftGridIslandN( mVegbmultS, fill=1 )
   mVegbmultWE <- shiftGridIslandE( mVegbmultW, fill=1 )
   mVegbmultNS <- shiftGridIslandS( mVegbmultN, fill=1 )   
   mVegbmultEW <- shiftGridIslandW( mVegbmultE, fill=1 )   
   
-                                   
+    
+  # above here can be put outside the function
+  ##############################################################################
+  
+  
   #calc arrivers in a cell from it's 4 neighbours
   #mArrivers <- pMove*(mN + mE + mS + mW)/4
   
