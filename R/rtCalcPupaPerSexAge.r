b269a2e7251043cb9837e4dc2e6fa8ce1672d936
AndySouth <southandy@gmail.com>
2015-03-30 13:57:21 +0100

    clarify comments on what it does

--- R/rtCalcPupaPerSexAge.r
+++ R/rtCalcPupaPerSexAge.r
@@ -17,23 +17,27 @@ rtCalcPupaPerSexAge <- function( pMortPupa = 0.25,
                                  iCarryCapF = 200)
 {
 
+#This follows hat-trick sheet:StabCalc c345
+#It is tricky
+ 
+#Effectively what it says is:
+#If a stable popn of 1300 females produce 100 larvae. 
+#If you want to get a carrying capacity of 4000 females you need to multiply 100 larvae by 4000/1300.  
 
-#to set num pupae per age class following hat-trick StabCalc c345
-#50*(1-pMortPupa)*pupDurF*andyCorrect4CarCap 
-
-#todo: refactor these nasty variable names
-#1 create a reference popn that produces 100 larvae
+  
+#1 find number of age0 females produced from 100 larvae of both sexes   
 fPopFAge0Ref <- 50*(1-pMortPupa)
-vPopFRef <- rtSetAgeStructure(vpMortF, fPopAge0=fPopFAge0Ref)
-fTotPopFRef <- sum(vPopFRef) 
-#fCorrect4CarryCap <- iCarryCapF / fTotPopFRef 
-#16/9/14 andy added ability to set starting popn by proportion of CC
-#here just influences the pupae, this effects adult age structure later
-fCorrect4CarryCap <- fStartPopPropCC * iCarryCapF / fTotPopFRef   
 
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# setting pupae ################
-#!BEWARE I'm not sure this is right yet
+#2 find stable age structure of females from this and age specific mortalities
+vPopFRef <- rtSetAgeStructure(vpMortF, fPopAge0=fPopFAge0Ref)
+
+#3 calculate total female population
+fTotPopFRef <- sum(vPopFRef) 
+
+#4 calculate a correction factor to scale up from the 100 larvae to get the desired carrying capacity
+fCorrect4CarryCap <- (iCarryCapF / fTotPopFRef) * fStartPopPropCC   
+
+#5 set the number of pupae using the correction
 fPupaPerSexAge <- fPopFAge0Ref*fCorrect4CarryCap
 
 
