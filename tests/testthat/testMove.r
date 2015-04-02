313a426d390030c7faf40f80fcb2e8fa5a07335a
AndySouth <southandy@gmail.com>
2015-04-02 15:23:14 +0100

    adding tests for new streamlined rtMove() and getting them to pass

--- tests/testthat/testMove.r
+++ tests/testthat/testMove.r
@@ -10,7 +10,7 @@ test_that("reduced movement down preferedness gradients works", {
   mMovers <- array(1,dim=dim(mVegCats))
   #note this doesn't include vegetation dependent movement (mVegMove)
   rtMoveReflectNoGoVegBoundary(mMovers, mVegCats=mVegCats, iBestVeg = 4)
-
+  rtMove(mMovers, mVegCats=mVegCats, iBestVeg = 4)
   
   #now how can I test a simple example where I know what I expect
   #in the e.g.s below iBestVeg set to 4 which is 'S' 
@@ -20,6 +20,9 @@ test_that("reduced movement down preferedness gradients works", {
   mVegCats <- array(c('B','B','B','B','S','B','B','B','B','B','B','B'),dim=c(3,4))  
   expect_equal( rtMoveReflectNoGoVegBoundary(mMovers, mVegCats=mVegCats, iBestVeg=4, pMove=0.4),
                 array(c(0, 0.03, 0, 0.03, 0.88, 0.03, 0, 0.03, 0, 0, 0, 0),dim=c(3,4))  )
+  #new streamlined rtMove()
+  expect_equal( rtMove(mMovers, mVegCats=mVegCats, iBestVeg=4, pMove=0.4),
+                array(c(0, 0.03, 0, 0.03, 0.88, 0.03, 0, 0.03, 0, 0, 0, 0),dim=c(3,4))  )
 
   #case : one central cell of preferred veg surrounded by veg of 2 less preferednesses
   #expect: movement to neighbours to be 0.1*pMove/4
@@ -27,14 +30,19 @@ test_that("reduced movement down preferedness gradients works", {
   mVegCats <- array(c('G','G','G','G','S','G','G','G','G','G','G','G'),dim=c(3,4))  
   expect_equal( rtMoveReflectNoGoVegBoundary(mMovers, mVegCats=mVegCats, iBestVeg=4, pMove=0.4),
                 array(c(0, 0.01, 0, 0.01, 0.96, 0.01, 0, 0.01, 0, 0, 0, 0),dim=c(3,4))  ) 
- 
+  #new streamlined rtMove()
+  expect_equal( rtMove(mMovers, mVegCats=mVegCats, iBestVeg=4, pMove=0.4),
+                array(c(0, 0.01, 0, 0.01, 0.96, 0.01, 0, 0.01, 0, 0, 0, 0),dim=c(3,4))  )  
+  
   #case : one central cell of 1more dense veg('O') surrounded by veg of 1less dense('B')
   #expect: movement to neighbours to be 1*pMove/4
   mMovers <- array(c(0,0,0,0,1,0,0,0,0,0,0,0),dim=c(3,4)) 
   mVegCats <- array(c('B','B','B','B','O','B','B','B','B','B','B','B'),dim=c(3,4))  
   expect_equal( rtMoveReflectNoGoVegBoundary(mMovers, mVegCats=mVegCats, iBestVeg=4, pMove=0.4),
                 array(c(0, 0.1, 0, 0.1, 0.6, 0.1, 0, 0.1, 0, 0, 0, 0),dim=c(3,4))  )
- 
+  #new streamlined rtMove()
+  expect_equal( rtMove(mMovers, mVegCats=mVegCats, iBestVeg=4, pMove=0.4),
+                array(c(0, 0.1, 0, 0.1, 0.6, 0.1, 0, 0.1, 0, 0, 0, 0),dim=c(3,4))  ) 
   
   
 })
@@ -51,17 +59,25 @@ test_that("vegetation dependent movement does what is expected", {
   mVeg <- mMovers
   expect_equal( rtMoveReflectNoGoVeg(mMovers, mNog, mVeg=mVeg, pMove=0.4),
                 array(c(0, 0, 0, 0.1, 0.7, 0.1, 0, 0.1, 0, 0, 0, 0),dim=c(3,4)) )
+  #new streamlined rtMove()
+  expect_equal( rtMove(mMovers, mNog, mVegMove=mVeg, pMove=0.4),
+                array(c(0, 0, 0, 0.1, 0.7, 0.1, 0, 0.1, 0, 0, 0, 0),dim=c(3,4)) )
   #as above but veg movement multiplier set to 2
   #expect 2* as many flies to move out
   mVeg <- mMovers*2
   expect_equal( rtMoveReflectNoGoVeg(mMovers, mNog, mVeg=mVeg, pMove=0.4),
-                array(c(0, 0, 0, 0.2, 0.4, 0.2, 0, 0.2, 0, 0, 0, 0),dim=c(3,4)) )  
+                array(c(0, 0, 0, 0.2, 0.4, 0.2, 0, 0.2, 0, 0, 0, 0),dim=c(3,4)) ) 
+  #new streamlined rtMove()
+  expect_equal( rtMove(mMovers, mNog, mVegMove=mVeg, pMove=0.4),
+                array(c(0, 0, 0, 0.2, 0.4, 0.2, 0, 0.2, 0, 0, 0, 0),dim=c(3,4)) )   
   #as above but veg movement multiplier set to 0.5
   #expect 0.5* as many flies to move out
   mVeg <- mMovers/2
   expect_equal( rtMoveReflectNoGoVeg(mMovers, mNog, mVeg=mVeg, pMove=0.4),
                 array(c(0, 0, 0, 0.05, 0.85, 0.05, 0, 0.05, 0, 0, 0, 0),dim=c(3,4)) )  
-  
+  #new streamlined rtMove()
+  expect_equal( rtMove(mMovers, mNog, mVegMove=mVeg, pMove=0.4),
+                array(c(0, 0, 0, 0.05, 0.85, 0.05, 0, 0.05, 0, 0, 0, 0),dim=c(3,4)) )   
   
   #now with 2 source cells and no nogo areas 
   mMovers <- array(c(0,0,0,0,1,0,0,1,0,0,0,0),dim=c(3,4))
@@ -71,13 +87,17 @@ test_that("vegetation dependent movement does what is expected", {
   mVeg <- array(c(0,0,0,0,2,0,0,0,0,0,0,0),dim=c(3,4))
   expect_equal( rtMoveReflectNoGoVeg(mMovers, mNog, mVeg=mVeg, pMove=0.4),
                 array(c(0, 0.2, 0, 0.2, 0.2, 0.2, 0, 1.2, 0, 0, 0, 0),dim=c(3,4)) ) 
-  
+  #new streamlined rtMove()
+  expect_equal( rtMove(mMovers, mNog, mVegMove=mVeg, pMove=0.4),
+                array(c(0, 0.2, 0, 0.2, 0.2, 0.2, 0, 1.2, 0, 0, 0, 0),dim=c(3,4)) )
+    
   #expect that when veg is 0.5 & 2 for neighbouring cells ...
   mVeg <- array(c(0,0,0,0,0.5,0,0,2,0,0,0,0),dim=c(3,4))
   expect_equal( rtMoveReflectNoGoVeg(mMovers, mNog, mVeg=mVeg, pMove=0.4),
                 array(c(0, 0.05, 0, 0.05, 1, 0.05, 0.2, 0.25, 0.2, 0, 0.2, 0),dim=c(3,4)) )   
-
-  
+  #new streamlined rtMove()
+  expect_equal( rtMove(mMovers, mNog, mVegMove=mVeg, pMove=0.4),
+                array(c(0, 0.05, 0, 0.05, 1, 0.05, 0.2, 0.25, 0.2, 0, 0.2, 0),dim=c(3,4)) )    
 })
 
 test_that("movement avoiding no-go areas indeed avoids no-go areas", {
@@ -88,30 +108,46 @@ test_that("movement avoiding no-go areas indeed avoids no-go areas", {
   mNog <- array(c(1,1,1,1,1,1,1,1,1,1,1,1),dim=c(3,4))
   expect_equal( rtMoveReflectNoGo(mMovers, mNog, pMove=0.4),
                 array(c(0, 0.1, 0, 0.1, 0.6, 0.1, 0, 0.1, 0, 0, 0, 0),dim=c(3,4)) )
+  #new streamlined rtMove()
+  expect_equal( rtMove(mMovers, mNog, pMove=0.4),
+                array(c(0, 0.1, 0, 0.1, 0.6, 0.1, 0, 0.1, 0, 0, 0, 0),dim=c(3,4)) )
   #1 nogo neighbour
   mNog <- array(c(1,0,1,1,1,1,1,1,1,1,1,1),dim=c(3,4))
   expect_equal( rtMoveReflectNoGo(mMovers, mNog, pMove=0.4),
+                array(c(0, 0, 0, 0.1, 0.7, 0.1, 0, 0.1, 0, 0, 0, 0),dim=c(3,4)) )
+  #new streamlined rtMove()
+  expect_equal( rtMove(mMovers, mNog, pMove=0.4),
                 array(c(0, 0, 0, 0.1, 0.7, 0.1, 0, 0.1, 0, 0, 0, 0),dim=c(3,4)) )
   #2 nogo neighbours
   mNog <- array(c(1,0,1,0,1,1,1,1,1,1,1,1),dim=c(3,4))
   expect_equal( rtMoveReflectNoGo(mMovers, mNog, pMove=0.4),
                 array(c(0, 0, 0, 0, 0.8, 0.1, 0, 0.1, 0, 0, 0, 0),dim=c(3,4)) )
+  #new streamlined rtMove()
+  expect_equal( rtMove(mMovers, mNog, pMove=0.4),
+                array(c(0, 0, 0, 0, 0.8, 0.1, 0, 0.1, 0, 0, 0, 0),dim=c(3,4)) )  
   #3 nogo neighbours
   mNog <- array(c(1,0,1,0,1,0,1,1,1,1,1,1),dim=c(3,4))
   expect_equal( rtMoveReflectNoGo(mMovers, mNog, pMove=0.4),
                 array(c(0, 0, 0, 0, 0.9, 0, 0, 0.1, 0, 0, 0, 0),dim=c(3,4)) )
+  #new streamlined rtMove()
+  expect_equal( rtMove(mMovers, mNog, pMove=0.4),
+                array(c(0, 0, 0, 0, 0.9, 0, 0, 0.1, 0, 0, 0, 0),dim=c(3,4)) )  
   #4 nogo neighbours, all flies stay
   mNog <- array(c(1,0,1,0,1,0,1,0,1,1,1,1),dim=c(3,4))
   expect_equal( rtMoveReflectNoGo(mMovers, mNog, pMove=0.4),
                 array(c(0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0),dim=c(3,4)) )
-  
+  #new streamlined rtMove()
+  expect_equal( rtMove(mMovers, mNog, pMove=0.4),
+                array(c(0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0),dim=c(3,4)) )  
   #reflection & nogo areas combined
   #mover in top left corner
   mMovers <- array(c(1,0,0,0,0,0,0,0,0,0,0,0),dim=c(3,4))
   mNog <- array(c(1,0,1,1,1,1,1,1,1,1,1,1),dim=c(3,4))
   expect_equal( rtMoveReflectNoGo(mMovers, mNog, pMove=0.4),
                 array(c(0.9, 0, 0, 0.1, 0, 0, 0, 0, 0, 0, 0, 0),dim=c(3,4)) )  
-  
+  #new streamlined rtMove()
+  expect_equal( rtMove(mMovers, mNog, pMove=0.4),
+                array(c(0.9, 0, 0, 0.1, 0, 0, 0, 0, 0, 0, 0, 0),dim=c(3,4)) )  
 })
 
 test_that("movement of multiple age classes using plyr::aaply works", {
@@ -146,10 +182,12 @@ test_that("movement of multiple age classes using plyr::aaply works", {
   #              array(c(0.9, 0, 0, 0.1, 0, 0, 0, 0, 0, 0, 0, 0),dim=c(3,4)) ) 
   
   #test use of move function in aaply above vs against using on it's own 
-  expect_equal( aGrid2[,,'F',1],
-                rtMoveReflectNoGo(aGrid[,,'F',1], mNog=mNog, pMove=0.4) )  
-  expect_equal( aGrid2[,,'M',2],
-                rtMoveReflectNoGo(aGrid[,,'M',2], mNog=mNog, pMove=0.4) )   
+  expect_equal( aGrid2[,,'F',1], rtMoveReflectNoGo(aGrid[,,'F',1], mNog=mNog, pMove=0.4) )  
+  expect_equal( aGrid2[,,'M',2], rtMoveReflectNoGo(aGrid[,,'M',2], mNog=mNog, pMove=0.4) )
+  #new streamlined rtMove()
+  expect_equal( aGrid2[,,'F',1], rtMove(aGrid[,,'F',1], mNog=mNog, pMove=0.4) )  
+  expect_equal( aGrid2[,,'M',2], rtMove(aGrid[,,'M',2], mNog=mNog, pMove=0.4) )  
+  
   
   #B) check the overall size of the popn doesn't change
   expect_equal( sum(aGrid), sum(aGrid2) )
@@ -162,31 +200,38 @@ test_that("reflective movement indeed reflects from boundaries", {
   mMovers <- array(c(0,0,0,0,1,0,0,0,0,0,0,0),dim=c(3,4))
   expect_equal( rtMoveReflect(mMovers, pMove=0.4),
                 array(c(0, 0.1, 0, 0.1, 0.6, 0.1, 0, 0.1, 0, 0, 0, 0),dim=c(3,4)) )
-  
+  expect_equal( rtMove(mMovers, pMove=0.4),
+                array(c(0, 0.1, 0, 0.1, 0.6, 0.1, 0, 0.1, 0, 0, 0, 0),dim=c(3,4)) )  
   #mover in middle of left boundary
   mMovers <- array(c(0,1,0,0,0,0,0,0,0,0,0,0),dim=c(3,4))
   expect_equal( rtMoveReflect(mMovers, pMove=0.4),
                 array(c(0.1, 0.7, 0.1, 0, 0.1, 0, 0, 0, 0, 0, 0, 0),dim=c(3,4)) )  
-  
+  expect_equal( rtMove(mMovers, pMove=0.4),
+                array(c(0.1, 0.7, 0.1, 0, 0.1, 0, 0, 0, 0, 0, 0, 0),dim=c(3,4)) )   
   #mover in top left corner
   mMovers <- array(c(1,0,0,0,0,0,0,0,0,0,0,0),dim=c(3,4))
   expect_equal( rtMoveReflect(mMovers, pMove=0.4),
                 array(c(0.8, 0.1, 0, 0.1, 0, 0, 0, 0, 0, 0, 0, 0),dim=c(3,4)) )  
-  
+  expect_equal( rtMove(mMovers, pMove=0.4),
+                array(c(0.8, 0.1, 0, 0.1, 0, 0, 0, 0, 0, 0, 0, 0),dim=c(3,4)) )   
   #mover in top right corner
   mMovers <- array(c(0,0,0,0,0,0,0,0,0,1,0,0),dim=c(3,4))
   expect_equal( rtMoveReflect(mMovers, pMove=0.4),
                 array(c(0, 0, 0, 0, 0, 0, 0.1, 0, 0, 0.8, 0.1, 0),dim=c(3,4)) )
-  
+  expect_equal( rtMove(mMovers, pMove=0.4),
+                array(c(0, 0, 0, 0, 0, 0, 0.1, 0, 0, 0.8, 0.1, 0),dim=c(3,4)) )  
   #mover in lower right corner
   mMovers <- array(c(0,0,0,0,0,0,0,0,0,0,0,1),dim=c(3,4))
   expect_equal( rtMoveReflect(mMovers, pMove=0.4),
                 array(c(0, 0, 0, 0, 0, 0, 0, 0, 0.1, 0, 0.1, 0.8),dim=c(3,4)) )
-
+  expect_equal( rtMove(mMovers, pMove=0.4),
+                array(c(0, 0, 0, 0, 0, 0, 0, 0, 0.1, 0, 0.1, 0.8),dim=c(3,4)) )
   #mover in lower left corner
   mMovers <- array(c(0,0,1,0,0,0,0,0,0,0,0,0),dim=c(3,4))
   expect_equal( rtMoveReflect(mMovers, pMove=0.4),
                 array(c(0, 0.1, 0.8, 0, 0, 0.1, 0, 0, 0, 0, 0, 0),dim=c(3,4)) )
+  expect_equal( rtMove(mMovers, pMove=0.4),
+                array(c(0, 0.1, 0.8, 0, 0, 0.1, 0, 0, 0, 0, 0, 0),dim=c(3,4)) )  
 })
 
 
