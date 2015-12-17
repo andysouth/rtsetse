5faa4234cafbb94bdf5da021dc8acf673f81ec91
AndySouth <southandy@gmail.com>
2015-12-17 20:43:02 +0000

    modify vignette

--- vignettes/vignette-eg1-sav-grass.R
+++ vignettes/vignette-eg1-sav-grass.R
@@ -23,11 +23,29 @@ rtPlotMapVeg(mVegCats)
 #load sim, no need to echo
 #may just lazyLoad so may not need this
 
-## ---- eval=TRUE, echo=FALSE, message=FALSE, fig.width=5.7, fig.height=2.6, fig.align='left'----
+## ---- eval=TRUE, echo=FALSE, message=FALSE, fig.width=5.7, fig.height=2.8, fig.align='left'----
+#```{r, eval=TRUE, echo=FALSE, message=FALSE, fig.width=5.7, fig.height=2.6, fig.align='left'}
 data(aRecord)
 
-days2plot <- c(1,2,3,10,20,40,60,80,100,110,120,150,300)
+days2plot <- c(1,2,3,10,20,40,60,80,100,110,120,150)
 
-tmp <- lapply(days2plot, function(x) rtPlotMapPop(aRecord, sex='M&F',days=x))
+#fMaxCellVal set to 300 so that all plots use same colour scheme
+#ext=c(10,40,10,40) to zoom in
+tmp <- lapply(days2plot, function(x) rtPlotMapPop(aRecord, sex='M&F', days=x, fMaxCellVal=300, ext=c(10,40,10,40)))
 
+
+## ---- eval=TRUE, echo=FALSE, message=FALSE, fig.width=5.7, fig.height=2.9, fig.align='left'----
+
+days2plot <- c(1,2,3,10,20,40,60,80,100,110,120,150)
+
+#fMaxCellVal set to 300 so that all plots use same colour scheme
+#ext=c(10,40,10,40) to zoom in
+tmp <- lapply(days2plot, function(x) rtPlotMapPop(aRecord, sex='M&F', days=x, ext=c(10,40,10,40), age=c(20:120)))
+
+
+## ---- echo=FALSE---------------------------------------------------------
+rtPlotPopGrid(aRecord)
+
+## ---- echo=FALSE---------------------------------------------------------
+rtPlotPopGrid(rtGetFromRecord(aRecord, age=c(20:120)))
 
