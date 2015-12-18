4058b1f3085aee5da3ade78c63c0b814d8379a39
AndySouth <southandy@gmail.com>
2015-12-18 10:48:51 +0000

    add doc for dataset

---
+++ R/datasets.r
@@ -0,0 +1,6 @@
+#' output from an example simulation run 
+#'
+#' popn by [days,y,x,sex,age] from a simulation run on a simple half grass, half savannah map 
+#'
+#' @format an array [days,y,x,sex,age]
+"aRecord"
