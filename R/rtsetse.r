d240fe501e0c3fbc8be7d5bb5cd094bcaf6d5f87
AndySouth <southandy@gmail.com>
2016-07-19 14:07:28 +0100

    text tweaks

--- R/rtsetse.r
+++ R/rtsetse.r
@@ -1,18 +1,24 @@
 #' simulating tsetse fly populations to investigate control options.
 #'
-#' rtsetse is being developed to provide functions to create tsetse fly population simulations with age, sex and spatial structure.  
+#' rtsetse provides functions to create tsetse fly population simulations with age, sex and spatial structure.
+#' 
+#' There is a User Interface that can be started by :
+#'    rt_UI()
+#'   
 #'
 #' The main functions to run simulations are :
 #'  \describe{
+#'   \item{rt_runGrid()}{grid simulation, landscape set from vegetation map, population starts at carrying capacity}
+#'   \item{rt_runGridTestSpread()}{grid simulation, homogenous landscape, spread from central populated cell}
 #'   \item{rt_runAspatial()}{age-structured a-spatial population simulation}
-#'   \item{rt_runGridTestSpread()}{grid simulation, homogenous landscape, spread fro central populated cell}
-#'   \item{rt_runGrid()}{grid simulation, grid simulation, landscape set from vegetation map, population starts at carrying capacity}
+
+
 #' }
 #' 
 #' @author
 #' Andy South \email{southandy@@gmail.com} 
 
-#' @import raster sp ggplot2
+#' @import raster sp ggplot2 shiny
 #' @importFrom dplyr group_by summarise %>%
 #' @docType package
 #' @name rtsetse
